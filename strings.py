04e027a402c5fef38ede77cdf8de0f09b9cfbe84
Abby Jones <abby.jones@avanade.com>
2022-10-19 16:55:46 +0100

    adding python strings file to repo

---
+++ strings.py
@@ -0,0 +1,12 @@
+# Write some code that uses some of the
+# String-concepts we have covered.
+print("hiiii")
+hello = "hello"
+print(type(hello))
+world = "world"
+print(hello + world)
+print(hello + " " + world)
+"hello", "world"
+name = "Abby"
+print(F"Hello {name}")
+print("Hello \nAbby")
